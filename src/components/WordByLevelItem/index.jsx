56be8499d3b06044ceae98ec428c3223b920cbc2
NataliaPnk <156689583+NataliaPnk@users.noreply.github.com>
2024-12-06 00:11:55 +0100

    toggle word

--- src/components/WordByLevelItem/index.jsx
+++ src/components/WordByLevelItem/index.jsx
@@ -1,18 +1,26 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { changeLangAction } from "../../store/reducers/wordsByLevelReducer";
 import s from "../WordByLevelItem/index.module.css";
 
-export default function WordByLevelItem({
-  id,
-  word_eng = "",
-  word_de = "",
-  lang,
-  status,
-  level = "",
-}) {
+export default function WordByLevelItem({ id }) {
+  const dispatch = useDispatch();
+
+  const word = useSelector((state) =>
+    state.wordsByLevel.find((el) => el.id === id)
+  );
+
+  if (!word) return null;
+
+  const displayWord = word.lang === "eng" ? word.word_eng : word.word_de;
+
+  const handleClick = () => {
+    dispatch(changeLangAction(id));
+  };
+
   return (
-    <div className={s.item}>
-      <p>{word_eng}</p>
-      <span>{status}</span>
+    <div className={s.item} onClick={handleClick}>
+      <p>{displayWord}</p>
     </div>
   );
 }
--- src/store/reducers/wordsByLevelReducer.js
+++ src/store/reducers/wordsByLevelReducer.js
@@ -1,7 +1,19 @@
 import { wordsData } from "../../data/wordsData";
 
+const CHANGE_LANG = "CHANGE_LANG";
+
+export const changeLangAction = (word_id) => ({
+  type: CHANGE_LANG,
+  payload: word_id,
+});
+
 export const wordsByLevelReducer = (state = wordsData, action) => {
-  if (action.type) {
-    return state;
+  if (action.type === CHANGE_LANG) {
+    return state.map((el) =>
+      el.id === action.payload
+        ? { ...el, lang: el.lang === "eng" ? "de" : "eng" }
+        : el
+    );
   }
+  return state;
 };
