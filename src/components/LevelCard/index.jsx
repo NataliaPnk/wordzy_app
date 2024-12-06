766d8d90a2ed4dfb2429a84d0beec8a48996ab7d
NataliaPnk <156689583+NataliaPnk@users.noreply.github.com>
2024-12-06 17:09:42 +0100

    style level names

--- src/components/LevelCard/index.jsx
+++ src/components/LevelCard/index.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import s from "../LevelCard/index.module.css";
+import { motion } from "motion/react";
 
 export default function LevelCard({ level_name }) {
   return (
-    <Link to={`/words/${level_name}`}>
-      <div>{level_name.toUpperCase()}</div>
-    </Link>
+    <motion.div
+      className={s.card}
+      whileHover={{ scale: 1.07 }}
+      whileTap={{ scale: 1.0 }}
+    >
+      <Link to={`/words/${level_name}`}>
+        <div>{level_name.toUpperCase()}</div>
+      </Link>
+    </motion.div>
   );
 }
